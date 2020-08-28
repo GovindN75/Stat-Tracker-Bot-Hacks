937213086a81fd2a94aa47ff2883137722603e7f
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-28 12:22:48 -0400

    api?

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+const { response } = require("express");
 
-router.get("/:platform/:gamertag", (req, res) => {
-  console.log(req.params.platform, req.params.gamertag);
-  res.send("AHAHAHAHAHAHAHAHAHAHAHHAHA");
+router.get("/:platform/:gamertag", async (req, res) => {
+  try {
+    const key = {
+      "TRN-Api-Key": process.env.TRACKER_KEY,
+    };
+    const { platform, gamertag } = req.params;
+    const repsonse = await fetch(
+      `${process.env.TRACKER_URL}/profile/${platform}/${gamertag}`,
+      { key }
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({
+      msg: "Server Error",
+    });
+  }
 });
 
 module.exports = router;
