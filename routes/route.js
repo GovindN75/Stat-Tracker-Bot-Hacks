290282fa4993266ed17791ba49548bc2d87f4d85
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-28 12:00:31 -0400

    get update

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
-router.get("/api/profile/:platform/:gamertag", (req, res) => {
+router.get("/:platform/:gamertag", (req, res) => {
   console.log(req.params.platform, req.params.gamertag);
   res.send("AHAHAHAHAHAHAHAHAHAHAHHAHA");
 });
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use("/api/profile", require("./routes/route"));
+
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
