e0c41d2c86109326c008716c7502d0b3b08163ad
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-28 15:50:15 -0400

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    =======
-    <nav class="navbar">
-      <a class="navbar-brand" href="#">
-        <img src="./assets/ow.png" style="width: 50px; height: 50px;" />
-      </a>
-    </nav>
-    >>>>>>> 21ee7f9f0985b0241f496b7cb341c6c6f2092f78
+    <Header />
   </div>
 </template>
 <script>
