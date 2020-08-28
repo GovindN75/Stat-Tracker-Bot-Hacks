44fb9564e4100cf0dd36048bc2c08f52414dcaa7
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-28 15:23:21 -0400

    bootstrap-vue, bootstrap, jquery, popperjs,

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
