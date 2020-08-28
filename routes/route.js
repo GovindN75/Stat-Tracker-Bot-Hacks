34f9897601b6e9a57605a43629f3ef85e1f304fb
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-28 11:58:27 -0400

    nodemon installed get request kind of set up

---
+++ routes/route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+router.get("/api/profile/:platform/:gamertag", (req, res) => {
+  console.log(req.params.platform, req.params.gamertag);
+  res.send("AHAHAHAHAHAHAHAHAHAHAHHAHA");
+});
+
+module.exports = router;
