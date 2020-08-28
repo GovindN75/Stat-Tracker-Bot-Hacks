cda37aa595ac0874b54d158521ec4dbc70722ef7
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-28 17:02:42 -0400

    getting info from api as soon as page loads

---
+++ client/src/components/Display.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="#app"></div>
+</template>
+
+<script>
+  import axios from "axios";
+  export default {
+    name: "display",
+    data() {
+      return {
+        loading: false,
+        error: null,
+        profileData: null,
+      };
+    },
+    async created() {
+      this.loading = true;
+      try {
+        const res = await axios.get(
+          `/api/v1/profile/${this.$route.params.platform}/${this.$route.params.gamertag}`
+        );
+        this.profileData = res.data.data;
+        console.log(this.profileData);
+        this.loading = false;
+      } catch (err) {
+        this.loading = false;
+        this.error = err.response.data.message;
+      }
+    },
+  };
+</script>
+
+<style scoped></style>
