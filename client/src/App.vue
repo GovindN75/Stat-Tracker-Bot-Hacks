2ce5172f3a19f361d7b0459ae0754a008fb4c405
danwei002 <danwei002@gmail.com>
2020-08-28 16:04:51 -0400

    Add background image

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
-    <Header />
+  <div class="min-vh-100" id="app">
+    <Header/>
   </div>
 </template>
+
 <script>
   import Header from "./components/Header";
   export default {
@@ -12,4 +13,11 @@
     },
   };
 </script>
-<style></style>
+
+<style>
+  #app {
+    background-image: url("./assets/oasis.jpg");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+</style>
