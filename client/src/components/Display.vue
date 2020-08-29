d10ae1bfe5e7782d064992899061d62c35f9d1c4
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-08-29 17:12:01 -0400

    Revert "Merge branch 'master' of https://github.com/GovindN75/Stat-Tracker-Bot-Hacks"
    This reverts commit e7a6a789063e4247908b69de7492beb90a51ef0b, reversing
    changes made to 79e6cc90fe948dc52d68f03694c5106de5002995.

--- client/src/components/Display.vue
+++ client/src/components/Display.vue
@@ -27,7 +27,7 @@
     </div>
 
     <div v-if="profileData">
-      <div class="container-fluid">
+      <div class="con">
         <div class="row">
           <h1 class="gamertag">
             {{ profileData.platformInfo.platformUserId }}
@@ -49,10 +49,29 @@
           </h1>
         </div>
       </div>
-      <div class="container-fluid">
-        <div class="row stats-wrapper">
+      <div class="stats-wrapper">
+        <div class="row">
           <div class="col-md-2">
             <h4 class="stats-title">Time Played</h4>
+          </div>
+          <div class="col-md-2">
+            <h4 class="stats-title">Win Rate</h4>
+          </div>
+          <div class="col-md-2">
+            <h4 class="stats-title">Time Spent On Fire</h4>
+          </div>
+          <div class="col-md-2">
+            <h4 class="stats-title">Time On Objective</h4>
+          </div>
+          <div class="col-md-2">
+            <h4 class="stats-title">Games Won</h4>
+          </div>
+          <div class="col-md-2">
+            <h4 class="stats-title">Most Eliminations</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2">
             <h5
               v-if="
                 profileData.segments[0].stats.timePlayed.value &&
@@ -70,8 +89,8 @@
             ></h5>
             <h5 v-else></h5>
           </div>
+
           <div class="col-md-2">
-            <h4 class="stats-title">Win Rate</h4>
             <h5
               v-if="
                 profileData.segments[0].stats.wlPercentage.value &&
@@ -105,8 +124,8 @@
             </h5>
             <h5 v-else class="stats-desc">N/A</h5>
           </div>
+
           <div class="col-md-2">
-            <h4 class="stats-title">Time Spent On Fire</h4>
             <h5
               v-if="
                 profileData.segments[0].stats.timeSpentOnFire.value &&
@@ -147,8 +166,8 @@
             </h5>
             <h5 v-else class="stats-desc">N/A</h5>
           </div>
+
           <div class="col-md-2">
-            <h4 class="stats-title">Time On Objective</h4>
             <h5
               v-if="
                 profileData.segments[0].stats.mostObjectiveTime.value &&
@@ -202,8 +221,8 @@
             </h5>
             <h5 v-else class="stats-desc">N/A</h5>
           </div>
+
           <div class="col-md-2">
-            <h4 class="stats-title">Games Won</h4>
             <h5
               v-if="
                 profileData.segments[0].stats.wins.value &&
@@ -236,8 +255,8 @@
             </h5>
             <h5 v-else class="stats-desc">N/A</h5>
           </div>
+
           <div class="col-md-2">
-            <h4 class="stats-title">Most Eliminations</h4>
             <h5
               v-if="
                 profileData.segments[0].stats.mostEliminations.value &&
@@ -441,6 +460,7 @@
   .stats-wrapper {
     background: rgba(0, 0, 0, 0.65);
     color: #fff;
+
     float: center;
     margin: 1rem auto;
     padding: 0.5rem 1.5rem -1.2rem;
